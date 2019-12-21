c9c7bdbd99f3e9f21d42f5b1bfe20ae18d2ddb7e
ajinkya <ajinkyaakap11@gmail.com>
2019-12-21 20:49:21 +0530

    main to sign up page intent created

--- app/src/main/java/com/example/books/sign_up.java
+++ app/src/main/java/com/example/books/sign_up.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class sign_up extends AppCompatActivity {
 
@@ -13,6 +15,15 @@ public class sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        Spinner branch =(Spinner)findViewById(R.id.branch);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.list_branch,android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        branch.setAdapter(adapter);
+
+        Spinner year =(Spinner)findViewById(R.id.year);
+        ArrayAdapter<CharSequence> adapter2=ArrayAdapter.createFromResource(this,R.array.list_year,android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        year.setAdapter(adapter2);
     }
 
 

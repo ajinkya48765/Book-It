a7b1fbe7f7d4e222c88b27b5be5790f0c2d01a2b
ajinkya <ajinkyaakap11@gmail.com>
2020-01-10 11:00:06 +0530

    public

---
+++ app/src/main/java/com/example/books/info.java
@@ -0,0 +1,14 @@
+package com.example.books;
+
+import android.widget.EditText;
+
+public class info {
+
+    public String name,email,phone;
+
+    public info(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
